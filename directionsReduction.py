d61c693a99e329d5c37eb504949255a78ee69138
IdryssPukke <salantean@gmail.com>
2024-01-11 12:07:05 +0100

    1.0.20

--- directionsReduction.py
+++ directionsReduction.py
@@ -4,13 +4,10 @@ def dir_reduc(arr):
     
     result = [] 
     for i in range(0,len(arr)): 
-        
         if not result: 
             result.append(arr[i]) 
-            
         elif result[-1] != opposites[arr[i]]: 
             result.append(arr[i]) 
-            
         else: 
             result.pop() 
             
@@ -20,9 +17,7 @@ def dir_reduc(arr):
 def dirReduc(arr):
     dir = " ".join(arr)
     dir2 = dir.replace("NORTH SOUTH",'').replace("SOUTH NORTH",'').replace("EAST WEST",'').replace("WEST EAST",'')
-    print(dir2)
     dir3 = dir2.split()
-    print(dir3)
     return dirReduc(dir3) if len(dir3) < len(arr) else dir3
 
 
