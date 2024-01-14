f84387fe010235397b854215a8a75e9b8dbd71e2
IdryssPukke <salantean@gmail.com>
2024-01-14 13:28:40 +0100

    1.0.14

--- which_are_in.py
+++ which_are_in.py
@@ -1,6 +1,16 @@
 """return words from one string that are in the second one"""
 
 def in_array(array1, array2):
+    """
+    return words from one string that are in the second one
+
+    Args:
+        array1 (array[string]): array of string
+        array2 (array[string]): array of string
+
+    Returns:
+        array: array of strings
+    """
     final = []
     for a1 in array1:
         for a2 in array2:
